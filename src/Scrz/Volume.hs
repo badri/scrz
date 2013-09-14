11ec8f8675da35e591679f0a03b92fa15986fdbf
Tomas Carnecky <tomas.carnecky@gmail.com>
2013-09-14 08:34:22 +0000

    Explicit exports in Volume module

--- src/Scrz/Volume.hs
+++ src/Scrz/Volume.hs
@@ -1,8 +1,12 @@
-module Scrz.Volume where
+module Scrz.Volume
+  (
+
+    allocateVolumes
+  , releaseVolumes
+
+  ) where
 
 import qualified Data.Map as M
-
-import System.Directory
 
 import Control.Monad
 import Control.Concurrent.STM
@@ -12,28 +16,37 @@ import Scrz.Utils
 import Scrz.Btrfs
 
 
-baseVolumeDirectory :: String
-baseVolumeDirectory = "/srv/scrz/volumes"
 
+------------------------------------------------------------------------------
+-- Exported symbols
+
+
+-- | Allocate all backing volumes for the given service. This function may
+-- throw an exception.
 allocateVolumes :: TVar Runtime -> Service -> IO [ BackingVolume ]
 allocateVolumes runtime service = do
     forM (serviceVolumes service) (allocateVolume runtime)
 
 
+-- Release all backing volumes.
 releaseVolumes :: TVar Runtime -> [ BackingVolume ] -> IO ()
 releaseVolumes runtime bv = forM_ bv (releaseVolume runtime)
 
 
-releaseVolume :: TVar Runtime -> BackingVolume -> IO ()
-releaseVolume _       (AdHocVolume _) = return ()
-releaseVolume runtime (ManagedVolume id') = do
-    atomically $ modifyTVar runtime $ \x ->
-        x { backingVolumes = M.delete id' (backingVolumes x) }
 
-    let path = baseVolumeDirectory ++ "/" ++ id'
-    btrfsSubvolDelete path
+------------------------------------------------------------------------------
+-- Private symbols
 
 
+baseVolumeDirectory :: String
+baseVolumeDirectory = "/srv/scrz/volumes"
+
+
+-- | Allocate a single backing volume. If no specific backing volume was
+-- specified, a new btrfs subvolume will be created. If the backing volume
+-- starts with a "/", then that is treated as an absolute path to an existing
+-- btrfs subvolume. Otherwise it is treated as the ID of an existing, managed
+-- backing volume.
 allocateVolume :: TVar Runtime -> Volume -> IO BackingVolume
 allocateVolume runtime volume = do
     case (volumeBacking volume) of
@@ -44,18 +57,27 @@ allocateVolume runtime volume = do
                 else do
                     rt <- atomically $ readTVar runtime
                     case M.lookup x (backingVolumes rt) of
-                        Nothing -> error $ "Backing volume not available"
+                        Nothing -> error $ "Backing volume not available: " ++ x
                         Just b -> return b
+
+
+-- | FIXME: Must not release the backing volume if it's a managed one and used
+-- by another container.
+releaseVolume :: TVar Runtime -> BackingVolume -> IO ()
+releaseVolume _       (AdHocVolume _) = return ()
+releaseVolume runtime (ManagedVolume id') = do
+    atomically $ modifyTVar runtime $ \x ->
+        x { backingVolumes = M.delete id' (backingVolumes x) }
+
+    btrfsSubvolDelete (baseVolumeDirectory ++ "/" ++ id')
 
 
 createBackingVolume :: TVar Runtime -> IO BackingVolume
 createBackingVolume runtime = do
     id' <- newId
-    let backingVolumePath' = baseVolumeDirectory ++ "/" ++ id'
-
-    btrfsSubvolCreate backingVolumePath'
-
     let ret = ManagedVolume id'
+
+    btrfsSubvolCreate (baseVolumeDirectory ++ "/" ++ id')
     atomically $ modifyTVar runtime $ \x ->
         x { backingVolumes = M.insert id' ret (backingVolumes x) }
 
